9279b8a35e2b1dd5819e6c14a38f603d5c3e161c
ahn-sj <jaesa5221@gmail.com>
2024-06-11 00:28:17 +0900

    init: 코딩테스트 요청 샘플 추가

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # problem-solving
 
-알고리즘 문제 풀이
+---
+
+## Guides
+
+### 1. input 설정
+
+1. run/debug configuration 설정
+2. modify options
+3. Redirect input
+4. input.txt 경로 설정
+5. input.txt에 `[input]` 설정
+
+![image](https://github.com/ahn-sj/algorithm/assets/64416833/1b2a35be-9ae5-450d-988a-b80e5457379b)
--- src/test/java/baekjoon/B2557.java
+++ src/test/java/baekjoon/B2557.java
@@ -2,11 +2,19 @@ package baekjoon;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class B2557 {
 
     @Test
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        final String input = br.readLine();
+
+        System.out.println("input = " + input);
     }
 
 }
