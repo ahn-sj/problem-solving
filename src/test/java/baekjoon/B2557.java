23cfd0dcadf16512b5b7da18834e286ab582d018
ahn-sj <jaesa5221@gmail.com>
2024-06-11 00:17:43 +0900

    init: 코딩테스트 테스트 샘플 추가

---
+++ src/test/java/baekjoon/B2557.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import org.junit.jupiter.api.Test;
+
+public class B2557 {
+
+    @Test
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
