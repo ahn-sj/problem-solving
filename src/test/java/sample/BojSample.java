1d75d2ff0d1177e8c38b5eadc78d630b2e124147
ahn-sj <jaesa5221@gmail.com>
2024-06-23 18:50:30 +0900

    refactor: 백준 샘플 파일 변경

--- src/test/java/sample/BojSample.java
+++ src/test/java/sample/BojSample.java
@@ -1,14 +1,11 @@
 package sample;
 
-import org.junit.jupiter.api.Test;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BojSample {
 
-    @Test
     public static void main(String[] args) throws IOException {
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
