1c0b24d003abc39966cde94c2dbcf9794f2a0f0b
ahn-sj <jaesa5221@gmail.com>
2024-06-25 23:59:23 +0900

    BOJ 18352. 특정 거리의 도시 찾기

---
+++ src/test/java/baekjoon/B18352.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B18352 {
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        final String input = br.readLine();
+
+        System.out.println("input = " + input);
+    }
+
+}
